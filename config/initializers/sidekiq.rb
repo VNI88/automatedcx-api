5992cd0ff4a12f3f8b2fdbb5c050e6c5dcc5b7f0
Vini Cavalcante <vinicavalcante@localhost.localdomain>
2020-06-11 16:56:50 -0300

    Fixes and test for AppStatusWorker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 
 Sidekiq.configure_server do |config|
-  redis_url = Rails.env == production? ENV['REDIS_PRODUCTION_URL'] : ENV['REDIS_STAGING_URL']
+  redis_url = Rails.env == 'production' ? ENV['REDIS_PRODUCTION_URL'] : ENV['REDIS_STAGING_URL']
   config.redis = { url: redis_url || 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  redis_url = Rails.env == production? ENV['REDIS_PRODUCTION_URL'] : ENV['REDIS_STAGING_URL']
-  config.redis = { redis_url || url: 'redis://localhost:6379/0' }
+  redis_url = Rails.env == 'production' ? ENV['REDIS_PRODUCTION_URL'] : ENV['REDIS_STAGING_URL']
+  config.redis = { url: redis_url || 'redis://localhost:6379/0' }
 end
