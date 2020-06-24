e914c8e53781b51a3955a8f149697fb847d8f6c2
Vini Cavalcante <vinicavalcante@localhost.localdomain>
2020-06-24 14:47:03 -0300

    Ignoring yarn on development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,6 @@ Rails.application.configure do
   Raven.configure do |config|
     config.dsn = 'https://88f9b757447b4c1d9bbcb6162dcf81de@o404238.ingest.sentry.io/5267631'
   end
+
+  config.webpacker.check_yarn_integrity = false
 end
