6d278d21b11db46e62b754afdfb7ae6c430d3549
Vini Cavalcante <vinicavalcante@localhost.localdomain>
2020-07-11 23:44:37 -0300

    Codacy fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(_resource_or_scope)
     rails_admin_url
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_url
   end
 end
