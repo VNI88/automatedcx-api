c586308049cf4e591b39562a94ff42fd169a340e
Vini Cavalcante <vinicavalcante@localhost.localdomain>
2020-07-24 21:18:15 -0300

    Fixing send grid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,8 +123,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password =>  ENV['SENDGRID_PASSWORD'],
-    :domain => 'gmail.com',
-    :address => 'automatedcxteam@gmail.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
