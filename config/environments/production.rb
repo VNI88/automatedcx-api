2712c24f05d16f84143034f8327c0b10827f6193
Vini Cavalcante <vinicavalcante@localhost.localdomain>
2020-07-30 21:23:24 -0300

    attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   end
 
   # Devise configuration
-  config.action_mailer.default_url_options = { host: ENV['STG_HOST'] || ENV['PRD_HOST'] }
+  config.action_mailer.default_url_options = { host: 'http://automatedcx-api-staging.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
