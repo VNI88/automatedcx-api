48aca66cd94d4ff3b177169c88ba8caabc2c6558
VNI88 <vini_cavalcanteee@hotmail.com>
2021-08-16 23:36:48 -0300

    Adding email message sender class

---
+++ app/services/message_sender/mailgun.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'mailgun-ruby'
+
+module MessageSender
+  # Class resposible for sending emails trough mailgun gem
+  class Mailgun
+    def initialize(from:, to:, subject:, text:)
+      @message = {
+        from: from,
+        to: to,
+        subject: subject,
+        text: text
+      }
+    end
+
+    def call
+      mailgun_client = ::Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+      mailgun_client.send_message(ENV['MAILGUN_DOMAIN'], @message)
+      service_response(success_flag: true, payload: @message)
+    rescue StandardError => e
+      Raven.capture_exception(e)
+      service_response(success_flag: false, error: e.message)
+    end
+
+    private
+
+    def service_response(success_flag:, payload: nil, error: nil)
+      OpenStruct.new(
+        {
+          success?: success_flag,
+          payload: payload,
+          error: error
+        }
+      ).to_h.delete_if { |_key, value| value.nil? }
+    end
+  end
+end
