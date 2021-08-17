4bb56c16ea3754177e8f34fcfa9ef1fee88b9ff3
VNI88 <vini_cavalcanteee@hotmail.com>
2021-08-16 23:35:47 -0300

    Improving telegram message sender class Some of rubocop points were analyzed and implemented.

--- app/services/message_sender/telegram.rb
+++ app/services/message_sender/telegram.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 
 module MessageSender
+  # Class responsible to send messages to Telegram users
   class Telegram
     def initialize(chat_id, message)
       @chat_id = chat_id
@@ -11,18 +14,24 @@ module MessageSender
       ::Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
         bot.api.send_message(chat_id: @chat_id, text: @message)
       end
-
-      OpenStruct.new({
-        success?: true,
-        payload: {
-          chat_id: @chat_id,
-          text: @message
-        }
-      })
     rescue StandardError => e
       Raven.capture_exception(e)
       Rails.logger.error(e)
-      OpenStruct.new({ success?: false, error: e })
+      OpenStruct.new({ success?: false, error: e.message })
+    end
+
+    private
+
+    def service_response
+      OpenStruct.new(
+        {
+          success?: true,
+          payload: {
+            chat_id: @chat_id,
+            text: @message
+          }
+        }
+      )
     end
   end
 end
