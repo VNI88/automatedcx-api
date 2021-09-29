965e99a47d0066f6d4341286ef1de0daf32bf837
VNI88 <vini_cavalcanteee@hotmail.com>
2021-09-28 22:03:38 -0300

    Fix outdated methods

--- app/workers/routines/action_worker.rb
+++ app/workers/routines/action_worker.rb
@@ -5,38 +5,37 @@ require 'sidekiq'
 
 module Routines
   # Responsible to execute routine actions
-  class ActionWorker
+  class ActionWorker < ::ApplicationWorker
     include Sidekiq::Worker
 
     sidekiq_options retry: false
 
     def perform(routine_id)
-      routine = Routine.find_by_id(routine_id)
-      execute(routine.action)
+      @routine = Routine.find(routine_id)
+      @recipient_list = @routine.recipient_list
+      @message_sender = @routine.message_sender
+      @notification = @routine.notification
+
+      Rails.logger.info("Creating sending event for routine_id: #{routine_id}")
+      create_sending_event
+
+      Rails.logger.info("Update notification - #{@notification.id} - status to: published")
+      @notification.update!(status: :published)
+      @event.update!(finished_at: Time.current)
+
+      Rails.logger.info("Creating service response event for routine_id: #{routine_id}")
+      execute_routine(@routine.action)
       routine.update_columns(status: :completed, updated_at: Time.current, finished_at: Time.current)
     rescue Routines::ActionWorkerError => e
-      routine&.update_columns(status: :error, updated_at: Time.current, finished_at: Time.current)
-      Raven.capture_exception(e)
+      @routine&.update!(status: :error, updated_at: Time.current, finished_at: Time.current)
+      Sentry.capture_exception(e)
     end
+  end
 
-    private
+  private
 
-    def execute_routine(action)
-      case action
-      when 'send_whatsapp_menssage'
-        metadata = JSON.parse(event_metadata(routine))
-        metadata['users'].map do |user|
-          message_body = "Hello #{user['name']},\n #{routine.message_template}"
-          MessageSender::Whatsapp.new(user['phone'], message_body).call
-        end
-      when 'send_email'
-        # TO DO create a mailer with this function
-      end
-    end
-
-    def event_metadata(routine)
-      Event.where(user_id: routine.user_id).where(next_event_name: routine.monitored_event).last.metadata
-    end
+  def execute_routine(action)
+    send(action)
   end
 
   class ActionWorkerError < StandardError; end
