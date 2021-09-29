2c3a7429c40fc2cd5f68c9d3235c40b4a0143657
VNI88 <vini_cavalcanteee@hotmail.com>
2021-09-28 21:18:27 -0300

    Add fields in portuguese into test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,10 @@ Rails.application.configure do
 
   config.cache_classes = false
 
+  config.i18n.fallbacks = true
+  config.i18n.available_locales = %w[pt en]
+  config.i18n.default_locale = :pt
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
