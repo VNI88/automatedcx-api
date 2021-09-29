38575fe29e2b8b3e5d14866486dd07bae7724d46
VNI88 <vini_cavalcanteee@hotmail.com>
2021-09-28 21:05:44 -0300

    Add Sentry initializer

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal
+
+Sentry.init do |config|
+  config.dsn = 'https://88f9b757447b4c1d9bbcb6162dcf81de@o404238.ingest.sentry.io/5267631'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.environment = %w[production staging development]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  config.release = 'v9.0.0'
+end
